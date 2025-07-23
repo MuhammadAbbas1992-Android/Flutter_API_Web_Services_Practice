27bd242c2c8af9ea0ba97caee16d044c42b05252
Muhammad Abbas <g.abbas1992developer@gmail.com>
2025-07-23 20:30:58 +0500

    This Application has coding to Implement multiple categories of APIs for given EndPoint of client/Servers ============================== 1) REST APIs Done ***************** i. GET(Get data) Done ii. POST (Upload data) Done iii. PUT (Fully update) Done iv. PATCH (Partially update) Done v. DELETE (Delete data) Done ============================== 2) Firebase Continue ***************** i. firebase_auth Done a. Login b. Signup c. Signin with email link for apps only not for web practice d. Forget or Resent Password ------------------------------ ii. firebase_core, firebase_database (Realtime Database as children) and firebase_storage (Store images) Continue ============================== iii. firebase_core, cloud_firestore (Cloud Firestore as collection) and firebase_storage (Store images) Continue

--- lib/custom_widgets/custom_text_widget.dart
+++ lib/custom_widgets/custom_text_widget.dart
@@ -5,16 +5,20 @@ class CustomTextWidget extends StatelessWidget {
     super.key,
     required this.text,
     this.color = Colors.white,
+    this.size,
+    this.fontFamily,
   });
 
   final String text;
   final Color color;
+  final double? size;
+  final String? fontFamily;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(color: color),
+      style: TextStyle(color: color, fontSize: size, fontFamily: fontFamily),
     );
   }
 }
--- lib/views/firebase_integrations/firebase_integration_screen.dart
+++ lib/views/firebase_integrations/firebase_integration_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_api_web_services_practice/custom_widgets/firebase_integration_widget.dart';
+import 'package:flutter_api_web_services_practice/res/constants/app_colors.dart';
+import 'package:flutter_api_web_services_practice/res/constants/app_fonts.dart';
 import 'package:flutter_api_web_services_practice/view_models/controllers/firebase_controllers/firebase_integration_screen_controller.dart';
 import 'package:flutter_api_web_services_practice/view_models/controllers/firebase_controllers/login_screen_controller.dart';
 import 'package:flutter_api_web_services_practice/view_models/controllers/firebase_controllers/sign_up_screen_controller.dart';
@@ -47,27 +49,40 @@ class _FirebaseIntegrationScreenState extends State<FirebaseIntegrationScreen> {
         body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                FirebaseIntegrationWidget(
-                  caseNo: 1,
-                  btnText: 'Login',
-                  categoryText: 'Login User',
-                  controller: firebaseIntegrationScreenController,
+            child: Card(
+              color: AppColors.tinGrey,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    const CustomTextWidget(
+                      text: 'Firebase Auth',
+                      color: AppColors.pink,
+                      size: 30,
+                      fontFamily: AppFonts.poppinsBold,
+                    ),
+                    const SizedBox(height: 10),
+                    FirebaseIntegrationWidget(
+                      caseNo: 1,
+                      btnText: 'Login',
+                      categoryText: 'Login User',
+                      controller: firebaseIntegrationScreenController,
+                    ),
+                    FirebaseIntegrationWidget(
+                      caseNo: 2,
+                      btnText: 'Sing up',
+                      categoryText: 'Sing up user',
+                      controller: firebaseIntegrationScreenController,
+                    ),
+                    FirebaseIntegrationWidget(
+                      caseNo: 3,
+                      btnText: 'Sing in with email link',
+                      categoryText: 'Sing in with email link',
+                      controller: firebaseIntegrationScreenController,
+                    ),
+                  ],
                 ),
-                FirebaseIntegrationWidget(
-                  caseNo: 2,
-                  btnText: 'Sing up',
-                  categoryText: 'Sing up user',
-                  controller: firebaseIntegrationScreenController,
-                ),
-                FirebaseIntegrationWidget(
-                  caseNo: 3,
-                  btnText: 'Sing in with email link',
-                  categoryText: 'Sing in with email link',
-                  controller: firebaseIntegrationScreenController,
-                ),
-              ],
+              ),
             ),
           ),
         ),
